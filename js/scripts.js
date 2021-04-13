6e47d80dbfedea9815ad624cafcca1ae9719463f
Mohamed Outeh <P2436635@my365.dmu.ac.uk>
2021-04-13 13:49:19 +0100

    mostly completed the mobile version of the houses page

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,11 @@ function myFunction(imgs) {
   expandImg.parentElement.style.display = "block";
 }
 
-function myFunction2(imgs) {
+function myFunction1(imgs) {
   // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
+  var expandImg = document.getElementById("expandImg");
   // Get the image text
-  var imgText = document.getElementById("imgtext");
+  var imgText = document.getElementById("imtext");
   // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
   // Use the value of the alt attribute of the clickable image as text inside the expanded image
