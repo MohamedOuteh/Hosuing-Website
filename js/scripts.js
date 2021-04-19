6967b55bb15b35b287dd8b6cf2e55138f3e75165
Mohamed Outeh <P2436635@my365.dmu.ac.uk>
2021-04-19 14:36:33 +0100

    Completed additional the styling of all the website

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,16 @@ function myFunction1(imgs) {
   // Show the container element (hidden with CSS)
   expandImg.parentElement.style.display = "block";
 }
+
+function myFunction2(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandIm");
+  // Get the image text
+  var imgText = document.getElementById("imtex");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
