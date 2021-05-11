4c0f58c375e9ae39b9e47d07d9d1d33432f359f6
Mohamed Outeh <P2436635@my365.dmu.ac.uk>
2021-05-11 14:25:57 +0100

    created new java page. Also added styling on the footer

---
+++ js/housescripts.js
@@ -0,0 +1,73 @@
+function myFunction(imgs){
+  // Get the expanded image
+  const expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  const imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+const expandImg = document.getElementById("expandedImg");
+const imgtext = document.getElementById("imgtext");
+expandImg.addEventListener('click', myFunction);
+imgtext.addEventListener('click', myFunction);
+
+const closebtn = document.getElementById("closebtn");
+closebtn.addEventListener("click", function(){
+	this.parentElement.style.display='none';
+});
+
+function myFunction1(imgs) {
+  // Get the expanded image
+  const expandImg = document.getElementById("expandImg");
+  // Get the image text
+  const imgText = document.getElementById("imtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+const image = document.getElementById("expandImg");
+const text = document.getElementById("imtext");
+image.addEventListener("click", myFunction1);
+text.addEventListener("click", myFunction1);
+
+const close = document.getElementById("close");
+close.addEventListener("click", function(){
+	this.parentElement.style.display='none';
+});
+
+function myFunction2(imgs) {
+  // Get the expanded image
+  const expandImg = document.getElementById("expandIm");
+  // Get the image text
+  const imgText = document.getElementById("imtex");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+const expandIm = document.getElementById("expandIm");
+const imtex = document.getElementById("imtex");
+expandIm.addEventListener("click", myFunction2);
+imtex.addEventListener("click", myFunction2);
+
+const buttonclose = document.getElementById("buttonclose");
+buttonclose.addEventListener("click", function(){
+	this.parentElement.style.display='none';
+});
+
+const print=document.getElementById("print");
+print.addEventListener("click", function()
+{
+	window.print();
+});
